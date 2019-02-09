78798039fba2bff89eb84729cb25ca58af84b28f
Nathan March <nathanzmarch@gmail.com>
2019-02-09 15:52:40 -0800

    update insert dod

--- www/pi1.php
+++ www/pi1.php
@@ -61,14 +61,14 @@
       // Grab query
   		$query = "INSERT INTO " . $_GET["typemenu"] . " VALUES (" . $curid . ", '" . $_GET["lname"] .
                 "', '" . $_GET["fname"] . "', '" . $_GET["sexmenu"] . "', "
-                . str_replace("-", "", $_GET["dob"]) . ", " . str_replace("-", "", $_GET["dob"]) . ");";
+                . str_replace("-", "", $_GET["dob"]) . ", " . str_replace("-", "", $_GET["dod"]) . ");";
   		// Sanitizing inputs actually breaks string matching for some reason
   		// $sanitized_query = mysql_real_escape_string($query, $db_connection);
   		$rs = mysql_query($query, $db_connection);
     }else{
       $query = "INSERT INTO " . $_GET["typemenu"] . " VALUES (" . $curid . ", '" . $_GET["lname"] .
                 "', '" . $_GET["fname"] . "', " . str_replace("-", "", $_GET["dob"])
-                . ", " . $_GET["dod"] . ");";
+                . ", " . str_replace("-", "", $_GET["dod"]) . ");";
   		// Sanitizing inputs actually breaks string matching for some reason
   		// $sanitized_query = mysql_real_escape_string($query, $db_connection);
   		$rs = mysql_query($query, $db_connection);
