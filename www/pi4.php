63acbc0fcc8c25ccfa2e39c046826152a953d22f
Nathan March <nathanzmarch@gmail.com>
2019-02-09 16:51:27 -0800

    pi5.php + post

--- www/pi4.php
+++ www/pi4.php
@@ -7,16 +7,17 @@
 	<h1 style="margin-top: 100px; margin-bottom: 0;">Add Movie Actor Relation</h1><br>
 	<h5>Please wait while movies and actors populate.</h5>
 
-	<form action="pi4.php" method="GET">
+	<form action="pi4.php" method="POST">
 		Actor:
 		<select name="actmenu">
 			<?php
-			if(isset($_GET["actmenu"])){
-					echo "<option selected> " . $_GET["actmenu"];
+			$db_connection = mysql_connect("localhost", "cs143", "");
+			mysql_select_db("CS143", $db_connection);
+			if(isset($_POST["actmenu"])){
+					echo "<option selected> " . $_POST["actmenu"];
 			}else{
 					echo "<option selected> --";
-					$db_connection = mysql_connect("localhost", "cs143", "");
-					mysql_select_db("CS143", $db_connection);
+
 					$query = "SELECT CONCAT(first, \" \", last) FROM Actor;";
 					$rs = mysql_query($query, $db_connection);
 
@@ -44,12 +45,10 @@
 			Movie:
 			<select name="movmenu">
 				<?php
-				if(isset($_GET["movmenu"])){
-						echo "<option selected> " . $_GET["movmenu"];
+				if(isset($_POST["movmenu"])){
+						echo "<option selected> " . $_POST["movmenu"];
 				}else{
 						echo "<option selected> --";
-						$db_connection = mysql_connect("localhost", "cs143", "");
-						mysql_select_db("CS143", $db_connection);
 						$query = "SELECT title FROM Movie;";
 						$rs = mysql_query($query, $db_connection);
 
@@ -76,14 +75,11 @@
    <br>
 
 	<?php
-	if(!isset($_GET["actmenu"]) and !isset($_GET["movmenu"])){
-	}else if(isset($_GET["actmenu"]) and isset($_GET["movmenu"])) {
-		// Connection init
-		$db_connection = mysql_connect("localhost", "cs143", "");
-		mysql_select_db("CS143", $db_connection);
+	if(!isset($_POST["actmenu"]) and !isset($_POST["movmenu"])){
+	}else if(isset($_POST["actmenu"]) and isset($_POST["movmenu"])) {
 
 		// Grab query
-		$name = split(" ", $_GET["actmenu"]);
+		$name = split(" ", $_POST["actmenu"]);
 		$query = "SELECT id FROM Actor WHERE first='$name[0]' and last='$name[1]';";
 		// Sanitizing inputs actually breaks string matching for some reason
 		// $sanitized_query = mysql_real_escape_string($query, $db_connection);
@@ -99,7 +95,7 @@
     }
 
 		// Grab query
-		$query = "SELECT id FROM Movie WHERE title ='" . $_GET["movmenu"] . "';";
+		$query = "SELECT id FROM Movie WHERE title ='" . $_POST["movmenu"] . "';";
 		// Sanitizing inputs actually breaks string matching for some reason
 		// $sanitized_query = mysql_real_escape_string($query, $db_connection);
 		$rs = mysql_query($query, $db_connection);
@@ -113,7 +109,7 @@
       }
     }
 		// Grab query
-		$query = "INSERT INTO MovieActor VALUES($movid, $actid, '" . $_GET["role"] ."');";
+		$query = "INSERT INTO MovieActor VALUES($movid, $actid, '" . $_POST["role"] ."');";
 		// Sanitizing inputs actually breaks string matching for some reason
 		// $sanitized_query = mysql_real_escape_string($query, $db_connection);
 		$rs = mysql_query($query, $db_connection);
